cbb6b070266bc1fef917b3cbf2b7f2f72cc09a64
najat <najatt.ismail@gmail.com>
2022-11-17 14:21:34 +0200

    xome design fixes

--- pages/shoppingcart/my-shopping-cart.js
+++ pages/shoppingcart/my-shopping-cart.js
@@ -10,7 +10,6 @@ import ProductsCategoriesNav from '../../components/products/products-categories
 import LangSwitch from '../../components/layout/langswitch'
 
 import { useCart } from "react-use-cart";
-// import Productfrom '../../components/products/products';
 
 function CartPage(props) {
   const categories = props.allcategories;
@@ -22,25 +21,79 @@ function CartPage(props) {
       updateItemQuantity,
       removeItem,
     } = useCart();
-  
-    if (isEmpty) return<Fragment>
-    <Head>
-    <title>My Cart</title>
-    <meta
-      name='description'
-      content=''
-    />
-    </Head>
-    <LangSwitch />
-    <Header />
-    <ProductsHeroSecion />
-    <ProductsCategoriesNav categories={categories}/>
+    let content;
+    if (isEmpty){
+      content =
     <div className="text-center">
-    <p>You don't have any products yet. Start adding some?</p>
+    <p>لا يوجد لديك منتجات في السلة.</p>
     </div>
-    <Footer />
-    </Fragment>;
-  
+    } else (
+      content = <div class="shadow-xl " style={{"margin-top": "-200px"}}>
+      <div class="py-6 px-4 sm:px-6">
+          <h1 class="text-lg font-bold">منتجاتك</h1>
+          <ul class="-my-6 divide-y divide-gray-200">
+            {items.map((item) => (
+            <li class="w-full flex flex-wrap md:flex-nowrap md:items-start items-center justify-between">
+              <div class="md:w-1/3 w-1/2 flex items-start justify-start">
+                <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border 
+                border-gray-200">
+                <Image width="885" height="891"
+                    src={item.image}
+                    alt={item.title}
+                    className="h-full w-full object-cover object-center group-hover:opacity-75"
+                /> 
+                </div>
+                <h3 className="mt-2 text-xl font-bold">{item.title}</h3>
+              </div>
+              <div class="md:w-1/3 w-1/2 flex flex-col items-end">
+                  <p className="text-lg font-medium text-gray-900">{item.price}</p>
+                  <div className='md:w-1/5 w-1/3 flex justify-ceendnter items-center 
+                  border border-gray-500'>
+                    <button class="w-1/3 h-full font-medium bg-orange-500 hover:bg-orange-700 text-white"
+                      onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
+                    >
+                      -
+                    </button>
+                    <p class="w-1/3 h-full text-center m-auto font-medium">{item.quantity}</p>
+                    <button class="w-1/3 h-full font-medium bg-orange-500 hover:bg-orange-700 text-white"
+                      onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
+                    >
+                      +
+                    </button>
+                </div>
+              </div>
+              <div class="md:w-1/3 w-full text-end">
+                    <button type="button" class="font-medium text-orange-700 hover:text-orange-500"
+                    onClick={() => removeItem(item.id)}>إزالة</button>
+              </div>
+            </li>
+            ))}
+            </ul>
+      </div>
+      <div class="border-t border-gray-200 py-6 px-4 sm:px-6">
+        <div class="flex justify-between text-base font-medium text-gray-900">
+        <p>السعر الإجمالي:</p>
+        <p>{cartTotal}</p>
+        </div>
+        <p class="mt-0.5 text-sm text-gray-500">الشحن والضرائب تحسب عند الخروج.</p>
+        <div class="mt-6">
+        <a href="#" class="flex items-center justify-center rounded-md border border-transparent 
+        bg-orange-500 px-6 py-3 text-base font-medium 
+        text-white shadow-sm hover:bg-orange-700">الشراء</a>
+        </div>
+        <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
+        <p>
+        <span class="font-medium">أو&nbsp; </span>
+            <button type="button" class="font-medium text-orange-500 hover:text-orange-700">
+            تابع التسوّق
+            <span aria-hidden="true"> &larr;</span>
+            </button>
+        </p>
+        </div>
+      </div>
+    </div>
+    )
+    
     return (
       <Fragment>
     <Head>
@@ -55,73 +108,10 @@ function CartPage(props) {
       <ProductsHeroSecion />
 
       <ProductsCategoriesNav categories={categories}/>
-<div class="shadow-xl " style={{"margin-top": "-200px"}}>
-  <div class="py-6 px-4 sm:px-6">
-      <h1 class="text-lg font-bold">منتجاتك</h1>
-      <ul class="-my-6 divide-y divide-gray-200">
-        {items.map((item) => (
-        <li class="w-full flex flex-wrap md:flex-nowrap md:items-start items-center justify-between">
-          <div class="md:w-1/3 w-1/2 flex items-start justify-start">
-            <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border 
-            border-gray-200">
-            <Image width="885" height="891"
-                src={item.image}
-                alt={item.title}
-                className="h-full w-full object-cover object-center group-hover:opacity-75"
-            /> 
-            </div>
-            <h3 className="mt-2 text-xl font-bold">{item.title}</h3>
-          </div>
-          <div class="md:w-1/3 w-1/2 flex flex-col items-end">
-              <p className="text-lg font-medium text-gray-900">{item.price}</p>
-              <div className='md:w-1/5 w-1/3 flex justify-ceendnter items-center 
-              border border-gray-500'>
-                <button class="w-1/3 h-full font-medium bg-orange-500 hover:bg-orange-700 text-white"
-                  onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
-                >
-                  -
-                </button>
-                <p class="w-1/3 h-full text-center m-auto font-medium">{item.quantity}</p>
-                <button class="w-1/3 h-full font-medium bg-orange-500 hover:bg-orange-700 text-white"
-                  onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
-                >
-                  +
-                </button>
-            </div>
-          </div>
-          <div class="md:w-1/3 w-full text-end">
-                <button type="button" class="font-medium text-orange-700 hover:text-orange-500"
-                onClick={() => removeItem(item.id)}>إزالة</button>
-          </div>
-        </li>
-        ))}
-        </ul>
-  </div>
-  <div class="border-t border-gray-200 py-6 px-4 sm:px-6">
-    <div class="flex justify-between text-base font-medium text-gray-900">
-    <p>السعر الإجمالي:</p>
-    <p>{cartTotal}</p>
-    </div>
-    <p class="mt-0.5 text-sm text-gray-500">الشحن والضرائب تحسب عند الخروج.</p>
-    <div class="mt-6">
-    <a href="#" class="flex items-center justify-center rounded-md border border-transparent 
-    bg-orange-500 px-6 py-3 text-base font-medium 
-    text-white shadow-sm hover:bg-orange-700">الشراء</a>
-    </div>
-    <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
-    <p>
-    <span class="font-medium">أو&nbsp; </span>
-        <button type="button" class="font-medium text-orange-500 hover:text-orange-700">
-        تابع التسوّق
-        <span aria-hidden="true"> &larr;</span>
-        </button>
-    </p>
-    </div>
-  </div>
-</div>
+    {content}
       <Footer />
     </Fragment>
-    );
+    )
 }
 export async function getStaticProps() {
   const categories = await axios.get("http://127.0.0.1:8000/api/categories");
