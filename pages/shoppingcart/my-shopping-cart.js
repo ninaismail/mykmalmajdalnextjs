54d6570aa1290a47fe9d5969d7d1f4548f3f530e
najat <najatt.ismail@gmail.com>
2022-11-17 13:48:50 +0200

    responsive cart

--- pages/shoppingcart/my-shopping-cart.js
+++ pages/shoppingcart/my-shopping-cart.js
@@ -60,8 +60,8 @@ function CartPage(props) {
       <h1 class="text-lg font-bold">منتجاتك</h1>
       <ul class="-my-6 divide-y divide-gray-200">
         {items.map((item) => (
-        <li class="flex items-start justify-between">
-          <div class="flex-1 flex items-start justify-start">
+        <li class="w-full flex flex-wrap md:flex-nowrap md:items-start items-center justify-between">
+          <div class="md:w-1/3 w-1/2 flex items-start justify-start">
             <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border 
             border-gray-200">
             <Image width="885" height="891"
@@ -72,9 +72,9 @@ function CartPage(props) {
             </div>
             <h3 className="mt-2 text-xl font-bold">{item.title}</h3>
           </div>
-          <div class="flex-1 flex flex-col items-end">
+          <div class="md:w-1/3 w-1/2 flex flex-col items-end">
               <p className="text-lg font-medium text-gray-900">{item.price}</p>
-              <div className='w-1/5 flex justify-ceendnter items-center 
+              <div className='md:w-1/5 w-1/3 flex justify-ceendnter items-center 
               border border-gray-500'>
                 <button class="w-1/3 h-full font-medium bg-orange-500 hover:bg-orange-700 text-white"
                   onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
@@ -89,7 +89,7 @@ function CartPage(props) {
                 </button>
             </div>
           </div>
-          <div class="flex-1 text-end">
+          <div class="md:w-1/3 w-full text-end">
                 <button type="button" class="font-medium text-orange-700 hover:text-orange-500"
                 onClick={() => removeItem(item.id)}>إزالة</button>
           </div>
@@ -98,25 +98,25 @@ function CartPage(props) {
         </ul>
   </div>
   <div class="border-t border-gray-200 py-6 px-4 sm:px-6">
-  <div class="flex justify-between text-base font-medium text-gray-900">
-  <p>السعر الإجمالي:</p>
-  <p>{cartTotal}</p>
-  </div>
-  <p class="mt-0.5 text-sm text-gray-500">الشحن والضرائب تحسب عند الخروج.</p>
-  <div class="mt-6">
-  <a href="#" class="flex items-center justify-center rounded-md border border-transparent 
-  bg-orange-500 px-6 py-3 text-base font-medium 
-  text-white shadow-sm hover:bg-orange-700">الشراء</a>
-  </div>
-  <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
-  <p>
-  <span class="font-medium">أو&nbsp; </span>
-      <button type="button" class="font-medium text-orange-500 hover:text-orange-700">
-      تابع التسوّق
-      <span aria-hidden="true"> &larr;</span>
-      </button>
-  </p>
-  </div>
+    <div class="flex justify-between text-base font-medium text-gray-900">
+    <p>السعر الإجمالي:</p>
+    <p>{cartTotal}</p>
+    </div>
+    <p class="mt-0.5 text-sm text-gray-500">الشحن والضرائب تحسب عند الخروج.</p>
+    <div class="mt-6">
+    <a href="#" class="flex items-center justify-center rounded-md border border-transparent 
+    bg-orange-500 px-6 py-3 text-base font-medium 
+    text-white shadow-sm hover:bg-orange-700">الشراء</a>
+    </div>
+    <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
+    <p>
+    <span class="font-medium">أو&nbsp; </span>
+        <button type="button" class="font-medium text-orange-500 hover:text-orange-700">
+        تابع التسوّق
+        <span aria-hidden="true"> &larr;</span>
+        </button>
+    </p>
+    </div>
   </div>
 </div>
       <Footer />
