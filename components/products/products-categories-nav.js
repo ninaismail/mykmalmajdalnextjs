79946eaff9b7381addc5439dfaa6f3f0724d8f7f
najat <najatt.ismail@gmail.com>
2022-11-18 11:34:30 +0200

    fixed another thing

--- components/products/products-categories-nav.js
+++ components/products/products-categories-nav.js
@@ -9,7 +9,7 @@ function ProductsCategoriesNav(props) {
   return (
 <div dir="rtl"className="relative right-0 bottom-[18em] flex flex-wrap justify-center">
 <div className="relative lg:flex-1 w-1/3 md:w-1/4 lg:w-1/7 w-full h-ful">
-    <Link href={allcategorieslink}>
+    <Link href={allcategorieslink} className="flex items-center justify-center">
        <Image src='/categories/allcategories.jpg' width="258" height="255" alt='جميع المنتجات' className="rounded-full mx-auto mb-15"/>
        <h2 className="absolute font-Roboto text-center font-bold md:text-4xl text-2xl">
        جميع المنتجات</h2>
