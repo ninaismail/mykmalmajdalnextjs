7446163b0a830cb96c8b74d6c4d3ea1b4ffa6979
najat <najatt.ismail@gmail.com>
2022-11-21 10:20:28 +0200

    sorting functionality in progress

--- components/products/products-filters.js
+++ components/products/products-filters.js
@@ -1,19 +1,50 @@
-import { useRef } from 'react';
+import { useEffect, useState } from 'react';
 import {BiChevronDown} from 'react-icons/bi'
 
 function ProductsFilters() {
+  const [data, setData] = useState([]);
+  const [sortType, setSortType] = useState('default');
+// call the API on initial load only
+ useEffect(() => {
+  fetchData();
+}, [sortType]);
 
-  return (
+const fetchData = async () => {
+  const products =  await fetch("http://127.0.0.1:8000/api/products");
+
+  const data = await products.json();
+  // using API response data as an input to sortData function
+  sortData(data)
+};
+
+// using data from parameter instead of state
+function sortData(data) {
+  let sortedData;
+  if (sortType === 'descending') {
+    sortedData = [...data].sort((a, b) => {
+      return b.price.localeCompare(a.price);
+    });
+  } else if (sortType === 'ascending') {
+    sortedData = [...data].sort((a, b) => {
+      return a.price.localeCompare(b.price);
+    });
+  } else {
+    return data;
+  }
+  setData(sortedData);
+}
+    return (
 <div className="shadow-xl md:w-1/6 sm:w-1/3 w-1/3 top-0 relative right-0 mx-6" style={{"margin-top": "-180px"}}>
 <select id="pricefilter" className="bg-orange-500 text-white border border-orange-500
-rounded-lg block w-full px-6 py-3">
-  <option className="bg-white text-black"selected>رتّب بحسب:</option>
-  <option className="bg-white text-black"value="pricehtol">السعر من الأعلى للأدنى</option>
-  <option className="bg-white text-black"value="priceltoh">السعر من الأدنى للأعلى</option>
+rounded-lg block w-full px-6 py-3" defaultValue='default'
+onChange={(e) => setSortType(e.target.value)}
+>
+  <option className="bg-white text-black"disabled value="default">رتّب بحسب:</option>
+  <option className="bg-white text-black"value="descending">السعر من الأعلى للأدنى</option>
+  <option className="bg-white text-black"value="ascending">السعر من الأدنى للأعلى</option>
 </select>
 <BiChevronDown color="white" size="30px" className='absolute left-2 top-2 z-100'/>
 </div>
   );
 }
-
 export default ProductsFilters;
