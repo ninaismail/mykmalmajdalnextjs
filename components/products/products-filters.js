332510437c8557c5cca2dd14eeb32fdecac50f4b
najat <najatt.ismail@gmail.com>
2022-11-21 10:54:44 +0200

    sorting functionality getting closer

--- components/products/products-filters.js
+++ components/products/products-filters.js
@@ -3,16 +3,17 @@ import {BiChevronDown} from 'react-icons/bi'
 
 function ProductsFilters() {
   const [data, setData] = useState([]);
-  const [sortType, setSortType] = useState('default');
+  const [sortPrice, setSortPrice] = useState('default');
 // call the API on initial load only
  useEffect(() => {
   fetchData();
-}, [sortType]);
+}, [sortPrice]);
 
 const fetchData = async () => {
   const products =  await fetch("http://127.0.0.1:8000/api/products");
 
   const data = await products.json();
+  console.log(data)
   // using API response data as an input to sortData function
   sortData(data)
 };
@@ -20,14 +21,18 @@ const fetchData = async () => {
 // using data from parameter instead of state
 function sortData(data) {
   let sortedData;
-  if (sortType === 'descending') {
+  if (sortPrice === 'descending') {
     sortedData = [...data].sort((a, b) => {
       return b.price.localeCompare(a.price);
     });
-  } else if (sortType === 'ascending') {
+    console.log("high to low")
+    console.log(sortedData)
+  } else if (sortPrice === 'ascending') {
     sortedData = [...data].sort((a, b) => {
       return a.price.localeCompare(b.price);
     });
+    console.log("low to high")
+    console.log(sortedData) 
   } else {
     return data;
   }
@@ -37,8 +42,7 @@ function sortData(data) {
 <div className="shadow-xl md:w-1/6 sm:w-1/3 w-1/3 top-0 relative right-0 mx-6" style={{"margin-top": "-180px"}}>
 <select id="pricefilter" className="bg-orange-500 text-white border border-orange-500
 rounded-lg block w-full px-6 py-3" defaultValue='default'
-onChange={(e) => setSortType(e.target.value)}
->
+onChange={(e) => setSortPrice(e.target.value)}>
   <option className="bg-white text-black"disabled value="default">رتّب بحسب:</option>
   <option className="bg-white text-black"value="descending">السعر من الأعلى للأدنى</option>
   <option className="bg-white text-black"value="ascending">السعر من الأدنى للأعلى</option>
