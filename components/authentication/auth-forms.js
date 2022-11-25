5deeacc1ccb94620f7eda902f762a6fe9da814dd
najat <najatt.ismail@gmail.com>
2022-11-25 14:19:20 +0200

    design

--- components/authentication/auth-forms.js
+++ components/authentication/auth-forms.js
@@ -4,9 +4,10 @@ import Register from "./registration-form"
   function AuthForms() {
     return (
   <div dir='rtl' className="" style={{"margin-top": "-100px"}}>
-      <h2 className="text-center font-Roboto font-bold md:text-4xl text-2xl"
-        >تسجيل جديد للشركات و الأفراد</h2>
-      <div className="w-3/4 mx-auto shadow-xl flex flex-wrap md:flex-nowrap items-start justify-center">
+      <h2 className="text-center font-Roboto font-bold md:text-4xl text-2xl">
+      لطلب الكميات بسعر الجملة للمصانع والشركات يرجى تعبئة بياناتك وسيتم التواصل معك قريباً!
+      </h2>
+      <div className="md:w-5/6 w-full mx-auto shadow-xl flex flex-wrap md:flex-nowrap items-start justify-center">
         <Login/>
         <Register/>
       </div>
